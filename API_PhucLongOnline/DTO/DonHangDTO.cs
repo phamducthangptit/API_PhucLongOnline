422d9bd4cbf5adb67b8417798888d07aede2d023
phamducthangptit <pthang2506@gmail.com>
2024-01-20 22:52:55 +0700

    update final

---
+++ API_PhucLongOnline/DTO/DonHangDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace API_PhucLongOnline.DTO
+{
+    public class DonHangDTO
+    {
+        public int IdDonHang { get; set; }
+        public DateTime NgayLap { get; set; }
+        public string TrangThaiDonHang { get; set; }
+    }
+}
